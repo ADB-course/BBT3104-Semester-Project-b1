86e110b0067e7fbd2ac49504e8aa08e0bc0e8073
kostakis2000 <139741686+kostakis2000@users.noreply.github.com>
2024-11-21 13:17:17 +0300

    This view summarizes delayed orders for each client, showing the count of delayed orders and the average delay duration.

--- 11_views/script.sql
+++ 11_views/script.sql
@@ -1,1 +1,10 @@
 -- Write your SQL code here
+-----Client Delivery Delays View----
+CREATE VIEW ClientDeliveryDelays AS
+SELECT 
+    client_id, 
+    COUNT(order_id) AS delayed_orders,
+    AVG(DATEDIFF(actual_delivery_date, scheduled_delivery_date)) AS avg_delay
+FROM `Order`
+WHERE delivery_status = 'Delayed'
+GROUP BY client_id;
