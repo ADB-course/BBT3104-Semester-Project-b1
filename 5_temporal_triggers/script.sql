116bb8a3485e6ac50fb0a2aae8538209fdcc0940
kostakis2000 <139741686+kostakis2000@users.noreply.github.com>
2024-11-21 13:09:24 +0300

    This trigger automatically logs orders that are delayed by comparing the actual delivery date with the scheduled date. It inserts delay details into a DelayedOrdersLog table.

--- 5_temporal_triggers/script.sql
+++ 5_temporal_triggers/script.sql
@@ -1,1 +1,11 @@
 -- Write your SQL code here
+-----Trigger for Logging Delayed Deliveries----
+CREATE TRIGGER log_delayed_orders
+AFTER UPDATE ON `Order`
+FOR EACH ROW
+BEGIN
+    IF NEW.actual_delivery_date > NEW.scheduled_delivery_date THEN
+        INSERT INTO DelayedOrdersLog (order_id, delay_duration, logged_date)
+        VALUES (NEW.order_id, DATEDIFF(NEW.actual_delivery_date, NEW.scheduled_delivery_date), NOW());
+    END IF;
+END;
